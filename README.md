1e7bbc7e231c7f3acb67015da8a6909fe6c06414
Mark Leusink <m.leusink@gmail.com>
2020-03-10 22:45:49 +0100

    open fakenames db in same folder as app db

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # simple-dql-explorer
-Simple DQL Explorer application
+
+Simple DQL Explorer application.
+
+Demo application to show how you can start using DQL in HCL Domino v10+. This demo is meant as a much more simple alternative to the DQL Explorer application that IBM provided (https://github.com/icstechsales/dql-explorer). You won't find any LotusScript or React in this one ;-). This version was built using a simple XPage and a Java backend.
+
+See my blog at https://linqed.eu for more information.
+
+The data used in this application was generated using https://www.fakenamegenerator.com/
+
+## Quickstart
+
+To use this demo application you'll need a HCL Domino v10 server (or higher). Nothing more.
+
+Copy this database and the fakenames database to your server in the same folder and you're good to go.
--- nsf/Code/Java/eu/linqed/dql/Utils.java
+++ nsf/Code/Java/eu/linqed/dql/Utils.java
@@ -1,18 +1,28 @@
 package eu.linqed.dql;
 
+import java.io.File;
+
 import com.ibm.xsp.extlib.util.ExtLibUtil;
 
 import lotus.domino.Database;
 import lotus.domino.Document;
 import lotus.domino.NotesException;
+import lotus.domino.Session;
 
 abstract class Utils {
 	
-	public static final String FAKENAMES_PATH = "dql/fakenames.nsf";
+	public static final String FAKENAMES = "fakenames.nsf";
 	
-	//get demo data database
+	//get demo data database (in the same folder as this one)
 	public static Database getFakenamesDb() throws NotesException {
-		return ExtLibUtil.getCurrentSession().getDatabase("", FAKENAMES_PATH);
+		Session s = ExtLibUtil.getCurrentSession();
+		String path = s.getCurrentDatabase().getFilePath();
+		if (path.indexOf(File.separator) >-1) {
+			path = path.substring(0, path.lastIndexOf(File.separator)) + File.separator;
+		} else {
+			path = "";
+		}
+		return s.getDatabase("", path + FAKENAMES);
 	}
 
 
